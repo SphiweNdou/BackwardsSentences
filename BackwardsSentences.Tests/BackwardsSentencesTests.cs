ea0eab9095138af8089332c6a30527f24cd414b5
Sphiwe Ndou <ndebondou@gmail.com>
2025-07-18 11:40:07 +0200

    Added null value input testing

--- BackwardsSentences.Tests/BackwardsSentencesTests.cs
+++ BackwardsSentences.Tests/BackwardsSentencesTests.cs
@@ -1,4 +1,5 @@
 using BackwardsSentences;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace BackwardsSentences.Tests
 {
@@ -8,9 +9,19 @@ namespace BackwardsSentences.Tests
         [InlineData("this is a test", "test a is this")]
         [InlineData("foobar", "foobar")]
         [InlineData("all your base", "base your all")]
+        [InlineData("  a  b   c  ", "c b a")]
+        [InlineData("", "")]
         public void ReverseSentence_wordOrder_reversedSentenced(string input, string expected)
         {
             Assert.Equal(expected, ReverseSentenceHelper.ReverseSentence(input));
+        }
+
+        [Fact]
+        public void ReverseSentence_NullInput_EmptyString()
+        {
+            string? input = null;
+            var result = ReverseSentenceHelper.ReverseSentence(input ?? string.Empty);
+            Assert.Equal(string.Empty, result);
         }
     }
 }
